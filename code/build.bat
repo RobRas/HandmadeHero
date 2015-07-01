da33158739159cb0b00646d583de1dd6a1b606ff
Robert Rasmussen <rsr47@nau.edu>
2015-07-01 10:50:43 -0700

    Stopped tracking code/build.bat

--- code/build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x64
-
-mkdir ..\..\build
-pushd ..\..\build
-cl -Zi ..\HandmadeHero\code\win32_handmade.cpp user32.lib
-popd
